05e88ef78816ea32f1bc974384634cd87fe698a4
Toni López Llaó <tonilopez450@gmail.com>
2026-08-24 20:17:58 +0100

    Fix Script SceneChanger

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -3,9 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    public void CambiarEscena(string Gat1)
+    // Hem canviat el nom de la variable perquè sigui més lògic
+    public void CambiarEscena(string nombreDeLaEscena)
     {
-        // Usamos la variable, NO el nombre fijo
-        SceneManager.LoadScene(Gat1);
+        SceneManager.LoadScene(nombreDeLaEscena);
     }
 }
