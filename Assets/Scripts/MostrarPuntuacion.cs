9f7661b6b8f5427c768b2df9892b596ca9a0b8a5
Toni López Llaó <tonilopez450@gmail.com>
2026-08-25 19:21:46 +0100

    Toggle Buttons y Global Score

---
+++ Assets/Scripts/MostrarPuntuacion.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro; // Esta línea es obligatoria para poder usar los textos de TextMeshPro
+
+public class MostrarPuntuacion : MonoBehaviour
+{
+    [Header("Arrastra aquí tu texto de la jerarquía")]
+    public TextMeshProUGUI textoScore;
+
+    // Usamos Update para que el texto se actualice constantemente al instante
+    void Update()
+    {
+        // Leemos la puntuación global de la memoria (si no hay nada guardado, será 0)
+        int scoreActual = PlayerPrefs.GetInt("ScoreGlobal", 0);
+
+        // Escribimos el número en el texto de la pantalla
+        textoScore.text = "Puntuación: " + scoreActual.ToString();
+    }
+}
