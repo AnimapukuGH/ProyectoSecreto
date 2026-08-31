cb1f18b8a98ac1c17944de2c73aade86d5d612ed
AnimapukuGH <animapuku@gmail.com>
2026-08-31 23:02:16 +0200

    aaaaaaaaaaaaa

---
+++ Assets/Sprites/Cursor.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PickNMore
+{
+    public class CursorUI : MonoBehaviour
+    {
+        [SerializeField]
+        private InputActionReference pointerPositionAction;
+
+        private RectTransform _cursorTransform;
+        private Canvas _parentCanvas;
+        private RectTransform _canvasRectTransform;
+        private Camera _canvasCamera;
+
+        private void Awake()
+        {
+            _cursorTransform = GetComponent<RectTransform>();
+            _parentCanvas = GetComponentInParent<Canvas>();
+
+            if (_parentCanvas != null)
+            {
+                _canvasRectTransform = _parentCanvas.GetComponent<RectTransform>();
+                _canvasCamera = _parentCanvas.renderMode == RenderMode.ScreenSpaceOverlay
+                    ? null
+                    : _parentCanvas.worldCamera;
+            }
+        }
+
+        private void Start()
+        {
+            if (_cursorTransform == null || _canvasRectTransform == null) return;
+
+            var mousePosition = Mouse.current.position.ReadValue();
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _canvasRectTransform, mousePosition, _canvasCamera, out var localPoint))
+            {
+                _cursorTransform.anchoredPosition = localPoint;
+            }
+        }
+
+        private void OnEnable()
+        {
+            Cursor.visible = false;
+            pointerPositionAction.action.performed += OnPointerPositionChanged;
+        }
+
+        private void OnDisable()
+        {
+            Cursor.visible = true;
+            pointerPositionAction.action.performed -= OnPointerPositionChanged;
+        }
+
+        private void OnPointerPositionChanged(InputAction.CallbackContext ctx)
+        {
+            if (_cursorTransform == null || _canvasRectTransform == null) return;
+
+            var mousePosition = ctx.ReadValue<Vector2>();
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _canvasRectTransform, mousePosition, _canvasCamera, out var localPoint))
+            {
+                _cursorTransform.anchoredPosition = localPoint;
+            }
+        }
+    }
+}
